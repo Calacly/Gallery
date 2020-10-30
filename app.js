53ece91ed166766065c747eccb1e77afb0d548ce
Calacly <54817827+Calacly@users.noreply.github.com>
2020-10-30 14:56:28 +0100

    rollback

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express"); //needs npm i express
-const fetch = require("node-fetch"); //normal fetch cannot be used in Node
+const express = require("express"); 
+const fetch = require("node-fetch"); 
 const ejs = require("ejs");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-require("dotenv").config(); //without setting it as a variable
+require("dotenv").config(); 
 
 
 //TODO: Add functionality to load more pages of photos
 //TODO: Add functionality to be able to click on a photo and have it flip the card with some info about the photo on the back   
 
 
-let imagesURLS = [];
+let photoData = [];
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
@@ -22,7 +23,9 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     
-    res.render("home", {imagesURLS: imagesURLS});
+    res.render("home", {
+        photoData: photoData,
+    });
       
 })
 
@@ -31,7 +34,7 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
     
-    const query = req.body.photoType; //tested: working
+    const query = req.body.photoType; 
     const apiKey = process.env.API_KEY;
     const url = "https://api.unsplash.com/search/photos?per_page=30&query=" + query + "&client_id=" + apiKey;
 
@@ -40,14 +43,15 @@ fetch(url)
 .then((response) => { 
  return response.json(); 
 }).then(data => {
-    imagesURLS = [];
-    data.results.forEach(photo => {
+    photoData = [];
+    
+    data.results.forEach(item => {
         
-        imagesURLS.push(photo.urls.small)
+        photoData.push(item)
         
         
     })  
-    res.redirect("/") // It is necessary to use res.redirct() at this level (inside .then(data)) try putting it just before the post() request closes and see what happens.
+    res.redirect("/") 
 })
 .catch(err => {console.log(`error: ${err}`);});
 })
